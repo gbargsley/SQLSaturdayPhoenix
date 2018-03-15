2a442ff0f94b829dd06f72319568c4e2e2c85dbe
Garry Bargsley <gbargsley@gmail.com>
2018-03-15 13:57:08 -0500

    updated demo file

--- gbargsley_DemoCode_SQLSaturdayPhoenix.ps1
+++ gbargsley_DemoCode_SQLSaturdayPhoenix.ps1
@@ -3,34 +3,12 @@ clear
 break
 
 
-
-
-
-
-
-
-
 # Load dbatools module
-# Update-Module dbatools -force
 Import-Module dbatools -force
-Import-Module C:\GitHub\dbatools\dbatools.psd1 -Force
-
-
-
-
-
-
 
 
 # Quick overview of commands
 Start-Process https://dbatools.io/commands
-
-
-
-
-
-
-
 
 
 # Set connection variables
@@ -44,27 +22,15 @@ $dev2016 = "localhost\dev2016"
 $prd2016 = "localhost\prd2016"
 
 
-
-
 # Get connections from Registered Servers (CMS) 
 $RegisteredServers = Get-DbaRegisteredServer -SqlInstance $CmsInstance
 $RegisteredServers | Select-Object ServerName
 
 
-
-
-
-
-
-
 # Max Memory Setting
-Get-DbaMaxMemory -SqlInstance $devServers
 Test-DbaMaxMemory -SqlInstance $SQLServers | Out-GridView
 Set-DbaMaxMemory -SqlInstance $SQLServers -MaxMB 1024
 Test-DbaMaxMemory -SqlInstance $SQLServers | Out-GridView
-
-
-
 
 
 # sp_configure settings
@@ -75,19 +41,8 @@ $destConfig = Get-DbaSpConfigure -SqlInstance $prd2016
 Compare-Object -ReferenceObject $sourceConfig -DifferenceObject $destConfig -Property DisplayName, RunningValue -PassThru | Sort-Object DisplayName | Select-Object DisplayName, RunningValue, ServerName
 
 
-
-
-
-
-
 # TempDB Configuration
 Test-DbaTempDbConfiguration -SqlInstance $dev2016 | Select-Object SqlInstance, Rule, Recommended, CurrentSetting, IsBestPractice | Out-GridView
-
-
-
-
-
-
 
 
 # Startup Parameters
@@ -95,30 +50,15 @@ Get-DbaStartupParameter -SqlInstance $dev2016
 Set-DbaStartupParameter -SqlInstance $dev2016 -TraceFlags 3226 -Confirm:$false
 
 
-
-
-
-
-
-
 # DBA Orphan Files
 $SQLServers | Find-DbaOrphanedFile
-
-
-
-
-
-
 
 
 # You can use the same JSON the website uses to check the status of your own environment
 $SQLServers | Get-DbaSqlBuildReference | Out-GridView
 $SQLServers | Test-DbaSqlBuild -MaxBehind 2CU | Out-GridView
 
-
 Start-Process https://sqlcollaborative.github.io/builds
-
-
 
 
 # SQL Agent Jobs
@@ -127,28 +67,9 @@ Get-DbaAgentJob -SqlInstance $dev2016 | Export-DbaScript -Path C:\temp\jobs.sql
 Start-Process C:\Temp\jobs.sql
 
 
-
-
-
-
-
-
 # Support Tools
 Install-DbaMaintenanceSolution -SqlInstance $SQLServers -Database DBA -CleanupTime 72 -BackupLocation C:\Temp -InstallJobs -ReplaceExisting 
 
 Install-DbaWhoIsActive -SqlInstance $SQLServers -Database DBA
-Invoke-DbaWhoIsActive -SqlInstance $prd2016 -ShowOwnSpid -ShowSystemSpids
 
 Install-DbaFirstResponderKit -SqlInstance $SQLServers -Database DBA
-
-
-
-
-# Check out our logs directory, so Enterprise :D
-Invoke-Item (Get-DbaConfig -FullName path.dbatoolslogpath).Value
-
-# Want to see what's in our logs?
-Get-DbatoolsLog | Out-GridView
-
-# Need to send us diagnostic information? Use this support package generator
-New-DbatoolsSupportPackage
